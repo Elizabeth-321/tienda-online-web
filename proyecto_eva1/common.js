dc63fd78fe815ade9a2b5a66edffc98692129a5e
Junior Zapata:) <yair.02.2003@gmail.com>
2026-09-23 23:17:32 -0300

    Carrusel completado!

--- proyecto_eva1/common.js
+++ proyecto_eva1/common.js
@@ -1,3 +1,4 @@
+
 /*  LOGICA COMPARTIDA: SESION,CATALOGO DE PRODUCTOS Y CARRITO DE COMPRA  */
 
 const KEY_SESION = "inv_sesion";
@@ -24,7 +25,7 @@ const SEED_PRODUCTOS = [
   { id: 7, codigo: "JW-007", nombre: "Pista de Autos Hot Wheels",   categoria: "Vehículos",     precio: 29990, stock: 20, activo: true,
     img: "https://placehold.co/400x300/ffe3b3/b35c00?text=Hot+Wheels" },
   { id: 8, codigo: "JW-008", nombre: "Rompecabezas 1000 Piezas",    categoria: "Juegos Mesa",   precio: 9990,  stock: 0,  activo: true,
-    img: "img/ChatGPT Image 23 sept 2026, 02_21_14 a.m.png" }
+    img: "https://placehold.co/400x300/d6ffe0/1a8a3f?text=Puzzle" },
 ];
 
 function leerSesion() {
